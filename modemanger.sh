6f5217f005488ddca8b28e2912649cb83445bcf1
SteezoMango <37036699+SteezoMango@users.noreply.github.com>
2019-03-24 05:19:03 -0500

    Add files via upload

---
+++ modemanger.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+iwconfig 
+
+printf "Which interface would you like to change?\n> "
+read iface
+while true;
+do
+    printf "0. Monitor\n1. Managed\n> "
+    read mode
+        if (($mode == 0));
+        then    
+            ifconfig $iface down
+            airmon-ng check kill
+            iwconfig $iface mode monitor
+            ifconfig $iface up
+            break
+
+        elif (($mode == 1));
+        then    
+            ifconfig $iface down		
+            airmon-ng check kill
+            iwconfig $iface mode managed
+            ifconfig $iface up
+            service network-manager start
+            break
+        else
+	        printf "Please choose a valid operator.\n"    
+        fi
+done	
