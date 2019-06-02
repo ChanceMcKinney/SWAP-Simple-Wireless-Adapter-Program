78d116d74de9b75dc920847ed6590e627b044b83
ChanceMcKinney <37036699+ChanceMcKinney@users.noreply.github.com>
2019-06-02 16:44:27 -0500

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,34 +1,66 @@
-#run as root
 #!/bin/bash
 
-clear
+if [ $EUID -ne 0 ]
+then
+    echo "Sorry, must run as root!"
+    exit 1
+fi
 
-iwconfig 
+monitor_mode () {
+    ifconfig $iface down
+    airmon-ng check kill
+    iwconfig $iface mode monitor
+    ifconfig $iface up
+}
 
-printf "Which interface would you like to change?\n> "
-read iface
+managed_mode () {
+    ifconfig $iface down        
+    airmon-ng check kill
+    iwconfig $iface mode managed
+    service network-manager start
+    ifconfig $iface up
+}
+
 while true;
 do
-    printf "0. Monitor\n1. Managed\n> "
-    read mode
-    if [ $mode = 0 ] 
-    then    
-        ifconfig $iface down
-        airmon-ng check kill
-        iwconfig $iface mode monitor
-        ifconfig $iface up
-        break
+    printf "\n"
 
-    elif [ $mode = 1 ]
-    then    
-        ifconfig $iface down		
-        airmon-ng check kill
-        iwconfig $iface mode managed
-        ifconfig $iface up
-        service network-manager start
-        break
+    /sbin/iwconfig
+
+    printf "\nWhich interface would you like to change?\n\n> "
     
-    else
-        printf "Please choose a valid operator.\n"    
-    fi
-done	
+    read iface
+    
+    while true;
+    do
+        printf "\n0. Monitor\n1. Managed\n\n> "
+        read mode
+       
+        if [ $mode = 0 ] 
+        then    
+            monitor_mode
+
+            if [ $? -eq 0 ]
+            then
+                notify-send "Monitor Mode enabled" "$iface" -t 4000
+                exit
+            fi
+            break
+           
+        elif [ $mode = 1 ]
+        then    
+           managed_mode
+           
+            if [ $? -eq 0 ]
+            then
+                 notify-send "Managed Mode enabled" "$iface" -t 4000
+                exit
+            fi
+            break
+        
+        else
+            printf "\nPlease choose a valid option.\n\n"    
+        fi
+    done	
+done
+        
